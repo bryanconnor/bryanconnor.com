dc2f2817e5dd8e9a9e15237ace079d60a7428225
Bryan Connor <bryan.j.connor@gmail.com>
2014-08-21 20:50:32 -0400

    changed project list on the homepage

--- site/snippets/worklist.php
+++ site/snippets/worklist.php
@@ -1,16 +1,17 @@
 <ul class="worklist">
 	<li>
+		<a href="<?php echo($site->url())?>/work/nomadic"><img src="<?php echo( $site->url())?>/assets/images/nomadic-snippet.png" /></a>
+		<a href="<?php echo($site->url())?>/work/nomadic"> Building a new platform for Nomadic.fm learners</a>
+	</li>
+	<!-- <li>
 		<a href="<?php echo($site->url())?>/work/opengov"><img src="<?php echo( $site->url())?>/content/work/opengov/madison-snippet.png" /></a>
 		<a href="<?php echo($site->url())?>/work/opengov">Hacking how bills become law with the OpenGov Foundation</a>
-	</li>
+	</li> -->
 	<li>
 		<a href="<?php echo($site->url())?>/work/commit"><img src="<?php echo( $site->url())?>/assets/images/commit-snippet.png" /></a>
 		<a href="<?php echo($site->url())?>/work/commit">Helping Commit! speak for Dallas area students</a>
 	</li>
-	<li>
-		<a href="<?php echo($site->url())?>/work/nomadic"><img src="<?php echo( $site->url())?>/assets/images/nomadic-snippet.png" /></a>
-		<a href="<?php echo($site->url())?>/work/nomadic"> Building a new platform for Nomadic.fm learners</a>
-	</li>
+	
 	<li>
 		<a href="<?php echo($site->url())?>/work/common-curriculum"><img src="<?php echo( $site->url())?>/assets/images/common-curriculum-snippet.png" /></a>
 		<a href="<?php echo($site->url())?>/work/common-curriculum">Putting Common Curriculum to the test with Baltimore teachers</a>
