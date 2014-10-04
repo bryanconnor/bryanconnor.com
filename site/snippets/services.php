677872bd0feabb513551932f83f415ac6a9390df
Bryan Connor <bryan.j.connor@gmail.com>
2014-10-03 21:48:03 -0400

    basic services on homepage

---
+++ site/snippets/services.php
@@ -0,0 +1,17 @@
+<ul class="services">
+	<li>
+		<h3>Envisioning</h3>
+		<p>Assistance in organizing early ideas into an array of visual forms to better explore and document the path ahead. We will explore not just the final product but why, and how it fits into the big picture.</p>
+	</li><li>
+		<h3>Prototyping</h3>
+		<p>Building something we can react to and share. Instead of designing how it looks we'll design how it works. Testing assumptions in interactive form will get us a long way towards a great product.</p>
+	</li>
+	<li>
+		<h3>Design</h3>
+		<p>We'll work toward a full set of planned and designed features with a final look and feel for the entire product. By gradually improving mockups or upgrading prototypes we can arriveat a production ready design system.</p>
+	</li><li>
+		<h3>Development</h3>
+		<p>Spanning the cricual gap between design and development I can help turn designs a reality in HTML, CSS and JavaScript. I'll gladly partner with an in-house dev team or additional freelancers to make the rest </p>
+	</li>
+</ul>
+<a href="mailto:workwith@bryanconnor.com" class="mail-bottom"><span class="typicons-mail"></span> Get In Touch</a>
