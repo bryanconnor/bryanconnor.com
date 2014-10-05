8598ed21d894b4edae9ca59279b4b01244c5402a
Bryan Connor <bryan.j.connor@gmail.com>
2014-10-04 23:46:37 -0400

    better services

--- site/snippets/services.php
+++ site/snippets/services.php
@@ -1,17 +1,22 @@
 <ul class="services">
 	<li>
 		<h3>Envisioning</h3>
-		<p>Assistance in organizing early ideas into an array of visual forms to better explore and document the path ahead. We will explore not just the final product but why, and how it fits into the big picture.</p>
+		
+		<p>
+			Laying the groudwork for successful products with design thinking from the start. I'll assist in organizing early ideas into an array of visual forms to better explore the path ahead. 
+			<!-- Organizing early ideas into an array of visual forms to better explore and document the path ahead. We will explore not just the form of final product but the why, and how it fits into the bigger picture. --></p>
 	</li><li>
 		<h3>Prototyping</h3>
 		<p>Building something we can react to and share. Instead of designing how it looks we'll design how it works. Testing assumptions in interactive form will get us a long way towards a great product.</p>
 	</li>
 	<li>
 		<h3>Design</h3>
-		<p>We'll work toward a full set of planned and designed features with a final look and feel for the entire product. By gradually improving mockups or upgrading prototypes we can arriveat a production ready design system.</p>
+		<p>
+			Designing how a product works from top to bottom. We'll work together to organize and pare down features for a more focused and effective product, always keepking empath with the end user as our guiding principle.
+		</p>
 	</li><li>
 		<h3>Development</h3>
-		<p>Spanning the cricual gap between design and development I can help turn designs a reality in HTML, CSS and JavaScript. I'll gladly partner with an in-house dev team or additional freelancers to make the rest </p>
+		<p>Spanning the critical gap between idea and execution. I can help convert designs to code with HTML, CSS and JavaScript. I'll gladly partner with an in-house dev team or additional freelancers to see the project through to the end.</p>
 	</li>
 </ul>
-<a href="mailto:workwith@bryanconnor.com" class="mail-bottom"><span class="typicons-mail"></span> Get In Touch</a>
+<div class="container" style="text-align: center;"><a href="mailto:workwith@bryanconnor.com" class="mail-bottom"><span class="typicons-mail"></span> Get In Touch</a></div>
