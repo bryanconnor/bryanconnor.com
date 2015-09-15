c073501e35dfd588f79a4d9b557406e712119cd0
Bryan Connor <bryan.j.connor@gmail.com>
2015-09-15 09:15:52 -0400

    tag list on permalink page

--- site/templates/visual.php
+++ site/templates/visual.php
@@ -3,6 +3,23 @@
 <div class="container">
 <section class="content">
 
+	<?php
+
+      // fetch all tags
+      $tags = $page->parent()->children()->visible()->pluck('tags', ',', true);
+
+    ?>
+    <ul class="tagcloud">
+      <?php foreach($tags as $tag): ?>
+      <li>
+        <a href="<?php echo url('blog/tag:' . $tag)?>">
+          <?php echo html($tag) ?>
+        </a>
+      </li>
+      <?php endforeach ?>
+      <li><a href="<?php echo $site->url() ?>/blog">everything</a></li>
+    </ul>
+    
 	<article class="visual-item">
       
       <h2><?php echo html($page->title()) ?></h2>
