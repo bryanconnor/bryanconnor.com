e359da0f314068f44b5376fdaae673f8cea03285
Bryan Connor <bryan.j.connor@gmail.com>
2016-04-17 17:03:43 -0400

    move all existing case studies to snippets (to make custom work pages more easily)

---
+++ site/snippets/nomadic-platform.php
@@ -0,0 +1,101 @@
+<div id="nomadic-platform" class="royalSlider rsMinW">
+  <div class="rsContent slide1 cover left-text">
+    <div class="text">
+      <h1>Building a new platform for Nomadic.fm learners</h1>
+      <span class="learn-more">Learn More</span>
+    </div>
+    <div class="image"><a class="rsImg" href="<?php echo url('content/work/nomadic/library-ipad-3.png') ?>"><a/></div>
+  </div>
+  <div class="rsContent slide2 centered-block">
+    <div class=" text " data-move-effect="none">
+      <p><a href="http://nomadic.fm" target="_blank">Nomadic.fm</a> is a digital learning company focused on preparing people to build, manage and lead businesses. They consult with academic and enterprise clients to build customized Field Manuals for students and deliver them through their platform.</p>
+      <p>I worked with the Nomadic team to improve the learning experience on the platform by giving students better tools and shifting focus towards the social elements of learning.</p>
+      <ul class="team">
+        <li><img src="<?php echo url('content/work/nomadic/team-tim.png'); ?>"/>Tim</li>
+        <li><img src="<?php echo url('content/work/nomadic/team-matt.png'); ?>"/>Matt</li>
+        <li><img src="<?php echo url('content/work/nomadic/team-junho.png'); ?>"/>Jun Ho</li>
+        <li><img src="<?php echo url('content/work/nomadic/team-bryan.png'); ?>"/>Bryan</li>
+        <li><div class="arr"></div><strong>My Role: </strong>Information Architecture &bull; UX Design &bull; UI Design &bull; Prototyping &bull; Front-end Development</li>
+      </ul>
+    </div>
+  </div>
+  <div class="rsContent slide-original left-text">
+    <div class="text ">
+      <h3>STARTING POINT</h3>
+      <p>The original platform laid a good foundation but didn’t feel like a coherent digital learning space. </p>
+    </div>
+    <div class="image "><img src="<?php echo url('content/work/nomadic/original-platform.gif') ?>" style="width: 90%;" /></div>
+  </div>
+  <div class="rsContent slide4 centered-block" >
+    <div class=" text " data-move-effect="none">
+      <p>Early in the design process we experimented with multiple approaches to each feature and reworked how they all fit together.</p>
+    </div>
+  </div>
+  <div class="rsContent slide-library left-text">
+    <div class="text ">
+      <h3>THE LIBRARY</h3>
+      <p>The first major improvement was a clear library of content. Field Manuals began to feel like independent elements, each with their own title, cover image, and level of completion.</p>
+      <aside><p>To read more about how Field Manuals are created, take a look at the <a href="">Nomadic CMS project</a>.</p></aside>
+    </div>
+    <div class="image "><img src="<?php echo url('content/work/nomadic/nomadic-bryan-connor-1.jpg') ?>" / ></div>
+  </div>
+  <div class="rsContent slide-favorites left-text">
+    <div class="text ">
+       <h3>HYPERTEXT</h3>
+      <p>While each field manual is self-contained, its elements are accessible in different ways throughout the platform. For example, text and video can be favorited for quick reference at a later date.</p>
+    </div>
+    <div class="image "><img src="<?php echo url('content/work/nomadic/favorites.png') ?>" style="width: 90%;" /></div>
+  </div>
+  <div class="rsContent slide-favorites left-text">
+    <div class="text ">
+       <h3>CONVERSATIONS</h3>
+      <p>Each Field Manual contains its own debate section but the conversation hub collects them all into one central location. It also allows users to start new conversations, outside of those set up by program administrators, so they can discuss the content on their own terms. </p>
+    </div>
+    <div class="image "><img src="<?php echo url('content/work/nomadic/conversation-ipad-2.png') ?>"  style="width: 90%;" /></div>
+  </div>
+  <div class="rsContent slide-notifications right-text">
+    <div class="text ">
+       <h3>NOTIFICATIONS</h3>
+      <p>Notifications help to bring users back to the platform, keeping them engaged with the discussions they’ve participated in.</p>
+    </div>
+    <div class="image "><img src="<?php echo url('content/work/nomadic/notifications.png') ?>" / ></div>
+  </div>
+  <div class="rsContent slide-leaderboard left-text">
+    <div class="text ">
+       <h3>LEADERBOARD</h3>
+      <p>All of the activity happening in and around field manuals contributes to a leaderbaord. This highlights the competitive aspects of learning in a group of your peers. </p>
+    </div>
+    <div class="image "><img src="<?php echo url('content/work/nomadic/leaderboard.gif') ?>" / ></div>
+  </div>
+  <div class="rsContent slide3 left-text">
+    <div class="text ">
+      <h3>FRONT-END DEVELOPMENT</h3>
+      <p>I also contributed heavily to the front-end development of the new platform. One of Nomadic's goals is to enable learning anywhere, so we engineered the platform to be just as responsive as the learning materials it houses. </p><p>I worked closely with <a href="https://github.com/junhonomad">Jun Ho</a> at Nomadic all the way through implementation, making adjustments as we went. Some of the front-end work was also completed with the help of the team at <a href="http://www.inthebackforty.com/">Back Forty</a>. 
+    </div>
+    <div class="image "><img src="<?php echo url('content/work/nomadic/library-responsive.png') ?>" / ></div>
+  </div>
+  <div class="rsContent slide-nomadic-closing centered-block">
+    <div class="text">
+      <p>The Nomadic platform is continually being improved and rolled out to new institutions and organizations around the globe. Below is a short list of other improvements we’ve made to the platform over time.</p>
+      <ul>
+        <li>
+          <img src="<?php echo url('content/work/nomadic/icon-welcome.png') ?>" /> 
+          Welcome page for students and administrators
+        </li>
+        <li>
+          <img src="<?php echo url('content/work/nomadic/icon-analytics.png') ?>" />
+          Analytics on learning and engagement for teachers and administrators
+        </li>
+        <li>
+          <img src="<?php echo url('content/work/nomadic/icon-notes.png') ?>" />
+          Teaching notes for content and quizzes used in classroom review
+        </li>
+        <li>
+          <img src="<?php echo url('content/work/nomadic/icon-upload.png') ?>" />
+          Dedicated resource management feature for uploaded documents
+        </li>
+      </ul>
+    </div>
+  </div>
+  
+</div>
---
+++ site/snippets/step.php
@@ -0,0 +1,74 @@
+<div id="step" class="royalSlider rsMinW">
+  <div class="rsContent step-slide-1 cover left-text">
+    <div class="text">
+      <h1>Designing one STEP at a time with Inquirium</h1>
+      <span class="learn-more">Learn More</span>
+    </div>
+    <div class="image"><a class="rsImg" href="<?php echo url('content/work/step/step-macbook-front-cmp.png') ?>"></a></div>
+  </div>
+  <div class="rsContent step-slide-2 centered-block">
+    <div class="text" data-move-effect="none">
+      <p><a href="https://www.steptool.org/" target="_blank">STEP</a> is a literary assessment tool that shows teachers what students have already learned, and what will get them to the next "step." I worked in partnership with Inquirium to build several versions of the tool for <a href="http://uchicagoimpact.org/" target="_blank">UChicago Impact</a>.</p>
+      <ul class="team">
+        <li><img src="<?php echo url('content/work/step/team-matt.jpg'); ?>"/>Matt</li>
+        <li><img src="<?php echo url('content/work/step/team-ben.jpg'); ?>"/>Ben</li>
+        <li><img src="<?php echo url('content/work/step/team-eric.jpg'); ?>"/>Eric</li>
+        <li><img src="<?php echo url('content/work/step/team-bryan.jpg'); ?>"/>Bryan</li>
+        <li><div class="arr"></div><strong>My Role: </strong>UX Design &bull; UI Design &bull; Front-end Development</li>
+      </ul>
+    </div>
+  </div>
+  <div class="rsContent step-slide-3 centered-block">
+    <div class="col-third">
+      <img src="<?php echo url('content/work/step/2011.gif') ?>" />
+      <p><strong>Version 1</strong> - A simpler web version of the tool.</p>
+    </div>
+    <div class="col-third">
+      <img src="<?php echo url('content/work/step/2012.gif') ?>" />
+      <p><strong>Version 2</strong> - Updated with more advanced visualizations .</p>
+    </div>
+    <div class="col-third">
+      <img src="<?php echo url('content/work/step/2013.gif') ?>" />
+      <p><strong>Version 3</strong> - A complete UI overhaul to incorperate rebranding by UChicago.</p>
+    </div>
+  </div>
+  <div class="rsContent step-slide-4 left-text">
+    <div class="text">
+      <h3>RESULTS</h3>
+      <p>The most recent version of the tool has hit its stride with intuitive visualizations that allow teachers to explore the results of reading assessments for individuals students, groups, classes, schools and districts.</p>
+    </div>
+    <div class="image "><a class="rsImg" href="<?php echo url('content/work/step/STEP-Bryan-Connor-1.png') ?>"> </a></div>
+  </div>
+  <div class="rsContent step-slide-5 left-text">
+    <div class="text">
+      <h3>MICRO &amp; MACRO</h3>
+      <p>In this visualization, students are represented by color coded rectangles, giving, at once, a broad and detailed view of student progress.</p>
+    </div>
+    <div class="image "><a class="rsImg" href="<?php echo url('content/work/step/STEP-Bryan-Connor-2.png') ?>"></a></div>
+  </div>
+  <div class="rsContent step-slide-6 left-text">
+    <div class="text">
+      <h3>STUDENT SNAPSHOT</h3>
+      <p>Clicking on an individual student reveals details of the last achieved step as well as the last assessment they didn’t pass. </p>
+    </div>
+    <div class="image "><a class="rsImg" href="<?php echo url('content/work/step/STEP-Bryan-Connor-3-cmp.png') ?>"></a></div>
+  </div>
+  <div class="rsContent step-slide-7 left-text">
+    <div class="text">
+      <h3>NEW COMPONENTS</h3>
+      <p>We've expanded the tool over time in keeping with the information architecture and visual style guide we set up. </p>
+    </div>
+    <div class="image "><a class="rsImg" href="<?php echo url('content/work/step/STEP-Bryan-Connor-4.gif') ?>"></a></div>
+  </div>
+  <div class="rsContent step-slide-8 left-text">
+    <div class="text">
+      <h3>REACH</h3>
+      <p>STEP tools and training models are currently being used in 21 states and 39 cities.</p>
+    </div>
+    <div class="image" style="position: relative; top: 12%;">
+      <div class="embed-container">
+        <iframe src="https://www.youtube.com/embed/bqUFmAw8w0s?rel=0" frameborder="0" allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
+</div>
